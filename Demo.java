9f88f7db4d319820cce5d92b016622b8a2902891
Snehal <69302530+SnehalSalve@users.noreply.github.com>
2024-03-16 23:19:43 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo{
+  public static void main(String args[]){
+    System.out.println("Hello World!!);
+
+    //variables in java
+    int a=5;
+    char c='s';
+    String s="Snehal";
+    System.out.ptintln("integer :"+a+"charachter:"+c+"string:"+s);
+  }
+}  
